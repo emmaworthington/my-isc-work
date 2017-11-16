01c3327ac2faee8b1e3a9bdb318d63465f42c0c1
emmaworthington <elw2u16@soton.ac.uk>
2017-11-16 09:23:50 +0000

    Added python 2.6/2.7 comments

--- python-work/sets.py
+++ python-work/sets.py
@@ -1,7 +1,14 @@
 # sets
 
+# this syntax will work with python 2.6 or 2.7
+# a = set([0, 1, 2, 3, 4, 5])
+# b = set([2, 4, 6, 8])
+
+# this syntax will only work with python 2.7
 a = {0, 1, 2, 3, 4, 5};
 b = {2, 4, 6, 8};
 
-print a | b
-print a & b
+print 'a = ', a
+print 'b = ', b
+print 'Union =', a | b
+print 'Intersection =', a & b
